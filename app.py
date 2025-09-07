8ac7a289110338c3dbb85d41256f89f975ecaba9
Moolya Swastik <109112771+Killmongers@users.noreply.github.com>
2025-09-08 00:22:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://swastikmoolya.42web.io"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
